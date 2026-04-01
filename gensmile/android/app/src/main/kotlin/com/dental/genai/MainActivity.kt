725f2cffe0c5dd40c1448ff7b31736d5702b1ccc
Maruf Mia <marufmia9070@gmail.com>
2026-04-01 21:13:17 +0600

    update

--- gensmile/android/app/src/main/kotlin/com/dental/genai/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.dental.genai
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- gensmile/lib/core/constant/app_constants.dart
+++
@@ -1,6 +0,0 @@
-class AppConstants {
-  AppConstants._();
-
-  static const String dbName  = '';
-  static const String appName = 'GenSmile';
-}
